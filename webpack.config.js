4f0d8d260ae7c7fb7dd62114c769ff4d9f281f94
Mike Choi <mike.choi@live.ca>
2018-10-20 00:35:07 -0700

    Added module property in webpack config: uses regex to id the JS files to be transpiled with babel-loader, and excludes node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "bundle.app.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["babel-preset-env"]
+                    }
+                }
+            }
+        ]
     }
 }
