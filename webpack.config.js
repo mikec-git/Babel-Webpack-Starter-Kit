8c4520fb226b7f158a46574dfc1c89b768c13964
Mike Choi <mike.choi@live.ca>
2018-10-20 01:14:49 -0700

    Finalized basic kit, add more later for styling and whatever else

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: ["@babel/polyfill", "./src/js/app.js"],
     output: {
         path: path.resolve(__dirname, "public"),
-        filename: "bundle.app.js"
+        filename: "bundle.js"
     },
     module: {
         rules: [
